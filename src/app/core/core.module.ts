75387aa77ae2472e2fbfb3aa4f419c63b836e60c
parth-sardhara-79 <parthsardhara026@gmail.com>
2019-08-28 17:06:59 +0530

    dropdown and modal inprogress.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { DatepickerComponent } from './datepicker/datepicker.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [DatepickerComponent],
+  declarations: [DatepickerComponent, DropdownComponent, ModalComponent],
   imports: [
     CommonModule,
     FormsModule,
     NgbModule.forRoot(),
 
   ],
-  exports:[DatepickerComponent]
+  exports:[DatepickerComponent, DropdownComponent, ModalComponent]
 })
 export class CoreModule { }
